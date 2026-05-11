8b2a3d3735b7eb1f26756525b71d6f3c30839c67
dreamcatcherkk <dreamcatcher.stays@gmail.com>
2026-05-11 17:48:15 +0100

    Create firebase-integration.js

---
+++ firebase-integration.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAL3_ct9JCeasfD4RYEwiOvqGVs-NN53g",
+  authDomain: "portfoliotracker-6250e.firebaseapp.com",
+  projectId: "portfoliotracker-6250e",
+  storageBucket: "portfoliotracker-6250e.firebasestorage.app",
+  messagingSenderId: "341928612090",
+  appId: "1:341928612090:web:26334a15e7ed7ce6f5b943"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
